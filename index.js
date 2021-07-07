52f1613f46c083f2885c908e72be49f085e530fb
Vincent Bélanger <vincent.belanger@nexusinno.com>
2021-07-07 19:14:01 -0400

    Add some more content

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bot = new eris.Client(process.env.BOT_TOKEN);
 const shouldReply = (msg) => !isOwnMessage(msg) && (isImageInCoreChannel(msg) || textContainsTrigger(msg));
 const isOwnMessage = (msg) => msg.author.id === bot.user.id;
 const isImageInCoreChannel = (msg) => msg.channel.name === 'core-player-quotes' && msg.attachments.length > 0;
-const textContainsTrigger = (msg) => msg.content.match(/enfant|femme|core|steph|docker/);
+const textContainsTrigger = (msg) => msg.content.match(/enfant|femme|core|steph|docker|sre|raymont|nexus/);
 
 const messages = [
   "Sorry gang! Je peux pas aujourd'hui, je m'occupe des enfants! Amusez-vous bien!!! :heart:",
@@ -14,6 +14,27 @@ const messages = [
   "J'ai un petit pincement au coeur car j'ai pas mal été \"l'architecte de solution par la bande et par proxy\" pendant quelques années lol mais bonne chances pour tes futurs défis! :heart:",
   'Je les connais pas eux, mais je suis willing de les connaitre!',
   "Je trippe sur plein d'affaires moi aussi, on va bien s'entendre je crois!",
+  "Désolé, je ne peux pas y être : je dois préparer et aller chercher mes enfants aujourd'hui, et c'est à 16h que ça se passe. Bienvenue chez toi, tu vas aimer l'activité :heart:",
+  "Moi j'ai pas de setup de même, j'ai juste des poids libres jusqu'à 50 lbs",
+  "Joyeux Nexusversaire!! Toé j'ai hâte de te voir en vrai! :slight_smile: (pis les autres aussi la!! Soyez pas jaloux :slight_smile: )",
+  "Pour ceux qui suivent l'actualité, un de mes amis est passé a la télé hier, la tornade a fait des dégats sur son terrain (et lui il est saie). P.S. Nous on va bien. tout est ok (la tornade a passé proche de chez moi mais aucun dégat)",
+  "Avant que vous nous posiez la question nous allons tous bien. La tornade a passé à quelques kilomètres de chez nous.",
+  "On peut t'aider avec ça!! Pu besoin de SQL Server local!",
+  "Perf. Mais sache que dans SRE on a fait du Docker pour conteneuriser votre env de dev pu besoin d'Avoir le sqlpackage.exe etc",
+  "Ok! Cest toi qui le sait! Anyway vous pouvez travailler avec le docker ou sans mais pour un dev vanille (genre Antoine Brisebois-Roy) ça l'accélère",
+  "Je joue pas à des shooters, sorry. J'ai moins de réflexes qu'avant (quoique je suis quand même pas pire à FFXlV et ça demande du bon réflexe quand même dans les raid)",
+  "Ici je compte venir à partir de Juillet, fort probablement 1 journée/semaine. J'ai hâte de vous revoir! :heart:",
+  "Bienvenue les boys !! Je reach out tout de suite, je suis seul avec les enfants aujourd'hui, il y a des chances que je ne puisse pas participer à l'activité",
+  "Je suis avec les enfants là",
+  "À la garderie... pour la dernière fois en 2 semaines lol... A+!",
+  "Mais ma femme travaille alors si les enfants ne dorment pas tout de suite ça se peut que je me connecte plus tard! Je vous tiendrai au courant ce soir",
+  "Mardi, ma femme travaille à 12h. Mais je peux me pointer au bureau si absolument nécessaire (si il y a un fuck de garderie je vais devoir partir par contre)",
+  "Ma femme a mal compris que je voulais travailler toute la journée ajd... j'ai dû dealer avec elle, je travaille jusqu'à midi, et je me reconnecte ce soir",
+  "Hey Sam, qu'est-ce qui arrive avec St-Hubert finalement? J'ai pe manqué un mémo!! Ce serait cool que l'équipe de dev fasse des poule requests!! Badum tsssss (joke copyright Ives Levi Diniz Rocha) :slight_smile:",
+  "Merci pour les infos et le formulaire! :heart: Aussi merci d'utiliser le mot distanciation physique et non distanciation sociale :heart:",
+  "J'ai un mac mais c'est + ma femme qui s'en sert... sorry moi non plus je peux pas t'aider",
+  "Merci pour ces 4 ans de confiance (souviens-toi, au début tu ignorais le contexte totalement). Cela veut donc dire que dans les premiers embauchés, je serai le seul qui reste. Je garde le fort!",
+  "J'ai finalement manqué tout ça... c'était la semaine de la gastro à la maison..j'ai manqué quoi? Qqn peut faire un recap?"
 ];
 
 function getRandomMessage() {
